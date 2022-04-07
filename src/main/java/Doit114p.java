c006f3425a2f513a12048bac016642ff23abc402
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-04-07 18:46:24 +0900

    Initial commit

---
+++ src/main/java/Doit114p.java
@@ -0,0 +1,12 @@
+public class Doit114p {
+    public static void main(String[] args) {
+
+        int i;
+        int sum;
+
+        for (i = 1, sum = 0; i < 11; i++) {
+            sum += i;
+        }
+        System.out.println("1부터 10까지의 합은" + sum + "입니다.");
+    }
+}
